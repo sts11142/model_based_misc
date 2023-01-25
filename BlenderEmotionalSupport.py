22a4b5da988f12e85f0a28959439063664ab608d
Shota Sato <last11142@gmail.com>
2023-01-26 00:11:12 +0900

    [fix]

--- BlenderEmotionalSupport.py
+++ BlenderEmotionalSupport.py
@@ -1227,13 +1227,13 @@ def evaluate(args, model: PreTrainedModel, tokenizer: PreTrainedTokenizer, eval_
                 strategy_logits = outputs.strategy_logits
 
             # print(strategy_logits.argmax(dim=-1))
-            print(emo_logits.shape)
-            print(emotion)
-            for idx, emo_logit in enumerate(emo_logits):
-                if emo_logit.argmax() == emotion[idx]:
-                    emo_hits.append(1)
-                else:
-                    emo_hits.append(0)
+            # print(emo_logits.shape)
+            # print(emotion)
+            # for idx, emo_logit in enumerate(emo_logits):
+            #     if emo_logit.argmax() == emotion[idx]:
+            #         emo_hits.append(1)
+            #     else:
+            #         emo_hits.append(0)
 
             # print(decoder_input_ids)
             # strategy_ids = decoder_input_ids[:, 0] - 54944
